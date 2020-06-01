aef982de95ffdfd56ad1f4d9c36bea456e3662c6
Florian Beeres <yuuki@protonmail.com>
2020-06-01 19:48:50 +0200

    initial commit

---
+++ functions/journal.fish
@@ -0,0 +1,88 @@
+#!/usr/bin/env fish
+
+# Initialize the data directory based on
+# https://wiki.archlinux.org/index.php/XDG_Base_Directory
+set data_dir
+if test -n "$FISH_JOURNAL_DIR"
+    set data "$FISH_JOURNAL_DIR"
+else if test -n "$XDG_DATA_DIR"
+    set data_dir "$XDG_DATA_DIR/fish_journal"
+else
+    set data_dir "$HOME/.local/share/fish_journal"
+end
+
+function __journal_entry_template
+    echo "#"
+    echo ""
+    echo "tags: "
+end
+
+function __journal_file_name
+    set -l d (date +"%G-%0m-%0d-%0k-%0M-%0S")
+
+    echo "$data_dir/$d.md"
+end
+
+function __journal_new
+    # Make sure directory exists and that it is a directory
+    if not test -d "$data_dir"
+        echo "Creating $data_dir to store journal entries"
+        mkdir -p "$data_dir"
+    end
+
+    if test \( -d "$data_dir" \) -a \( -f "$data_dir" \)
+        echo "$data_dir is a file, exiting"
+        exit 1
+    end
+
+    set -l fname (__journal_file_name)
+    touch "$fname"
+    set -l template __journal_entry_template 
+    "$template" >> "$fname"
+
+    # Store template in temporary file for easy check
+    # if user actually edited the default template.
+    # If not, delete the entry again
+    set -l tmpfile (mktemp)
+    "$template" > "$tmpfile"
+
+    if not test -n "$EDITOR" 
+        echo '$EDITOR not set, exiting'
+        exit 1
+    end
+    "$EDITOR" "$fname"
+
+    if cmp "$fname" "$tmpfile"
+        echo "You didn't change the default template, so I'll delete the entry again"
+        rm "$fname"
+    else
+        echo "Created new journal entry $fname"
+    end
+end
+
+function __journal_list_tags
+    # Gather tags but without the leading 'tags: '
+    # and skip duplicates
+    set -l tags
+    for line in (grep -hR "tags:" "$data_dir")
+        for t in (string split " " "$line")
+            if not test (string trim "$t") = "tags:";
+                and not contains $t $tags
+                set -a tags "$t"
+            end
+        end
+    end
+
+    for t in $tags
+        echo $t
+    end
+end
+
+switch "$argv[1]"
+    case "tags"
+        __journal_list_tags
+    case ""
+        __journal_new
+end
+
+# // vim: set ft=fish:
