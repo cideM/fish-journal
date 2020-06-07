df003df7811e56bb094aa17d3155d1cc86946cc9
Florian Beeres <yuuki@protonmail.com>
2020-06-07 21:37:12 +0200

    Better variable initialization
    Cleans up initialization of variables such as XDG_DATA_DIR and
    FISH_JOURNAL_DIR

--- functions/journal.fish
+++ functions/journal.fish
@@ -2,14 +2,8 @@
 
 # Initialize the data directory based on
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory
-set data_dir
-if test -n "$FISH_JOURNAL_DIR"
-    set data "$FISH_JOURNAL_DIR"
-else if test -n "$XDG_DATA_DIR"
-    set data_dir "$XDG_DATA_DIR/fish_journal"
-else
-    set data_dir "$HOME/.local/share/fish_journal"
-end
+set -q XDG_DATA_DIR; or set XDG_DATA_DIR "$HOME/.local/share"
+set -q FISH_JOURNAL_DIR; or set FISH_JOURNAL_DIR "$XDG_DATA_DIR/fish_journal"
 
 function __journal_entry_template
     echo "#"
@@ -20,18 +14,18 @@ end
 function __journal_file_name
     set -l d (date +"%G-%0m-%0d-%0k-%0M-%0S")
 
-    echo "$data_dir/$d.md"
+    echo "$FISH_JOURNAL_DIR/$d.md"
 end
 
 function __journal_new
     # Make sure directory exists and that it is a directory
-    if not test -d "$data_dir"
-        echo "Creating $data_dir to store journal entries"
-        mkdir -p "$data_dir"
+    if not test -d "$FISH_JOURNAL_DIR"
+        echo "Creating $FISH_JOURNAL_DIR to store journal entries"
+        mkdir -p "$FISH_JOURNAL_DIR"
     end
 
-    if test \( -d "$data_dir" \) -a \( -f "$data_dir" \)
-        echo "$data_dir is a file, exiting"
+    if test \( -d "$FISH_JOURNAL_DIR" \) -a \( -f "$FISH_JOURNAL_DIR" \)
+        echo "$FISH_JOURNAL_DIR is a file, exiting"
         exit 1
     end
 
@@ -69,7 +63,7 @@ function __journal_search
     if test -n "$_flag_t"
         set -l tags (string split "," "$_flag_t")
         set -l pattern (string join '|' $tags)
-        for match in (grep -R -Ewo -- $pattern $data_dir | awk -F ':' '{ print $1 }' | sort | uniq)
+        for match in (grep -R -Ewo -- $pattern $FISH_JOURNAL_DIR | awk -F ':' '{ print $1 }' | sort | uniq)
             cat $match
             echo ""
         end
@@ -82,7 +76,7 @@ function __journal_list_tags
     # Gather tags but without the leading 'tags: '
     # and skip duplicates
     set -l tags
-    for line in (grep -hR "tags:" "$data_dir")
+    for line in (grep -hR "tags:" "$FISH_JOURNAL_DIR")
         for t in (string split " " "$line")
             if not test (string trim "$t") = "tags:";
                 and not contains $t $tags
