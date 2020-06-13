48ac85881d5fd79f5182bee278b9d65cafad343a
Florian Beeres <yuuki@protonmail.com>
2020-06-13 19:25:20 +0200

    Major rework
    - Store entries as directories but random names and one file for title,
      tags, text and date
    - Fully implement search and sort search results

--- functions/journal.fish
+++ functions/journal.fish
@@ -5,16 +5,18 @@
 set -q XDG_DATA_DIR; or set XDG_DATA_DIR "$HOME/.local/share"
 set -q FISH_JOURNAL_DIR; or set FISH_JOURNAL_DIR "$XDG_DATA_DIR/fish_journal"
 
+set __fish_journal_date_format "%Y-%m-%d %T"
+
 function __journal_entry_template
-    echo "#"
     echo ""
-    echo "tags: "
 end
 
-function __journal_file_name
-    set -l d (date +"%G-%0m-%0d-%0k-%0M-%0S")
-
-    echo "$FISH_JOURNAL_DIR/$d.md"
+# TODO: Add -T --title option
+# TODO: Add -d --date option (use human language insofar date supports it)
+# TODO: Editor opens only for body of entry now
+# TODO: store 4 files: title, body, tags, date. Folder name is date + lower cased and escaped title probably style=var (check man string escape)
+function __journal_dir_name
+    echo "$FISH_JOURNAL_DIR"/(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 10)
 end
 
 function __journal_new
@@ -29,47 +31,122 @@ function __journal_new
         exit 1
     end
 
-    set -l fname (__journal_file_name)
-    touch "$fname"
-    set -l template __journal_entry_template 
-    "$template" >> "$fname"
+    set -l entry_dir (__journal_dir_name)
+    mkdir -p "$entry_dir"
 
-    # Store template in temporary file for easy check
-    # if user actually edited the default template.
-    # If not, delete the entry again
+    set -l options                              \
+        (fish_opt -s t -l tags -r --multiple-vals) \
+        (fish_opt -s T -l title -r)                \
+        (fish_opt -s d -l date -r)
+    argparse $options -- $argv
+
+    # Store date
+    if set -q _flag_d
+        echo (date -u -d "$_flag_d" +$__fish_journal_date_format) > $entry_dir/date
+    else
+        echo (date -u +$__fish_journal_date_format) > $entry_dir/date
+    end
+
+    # Store tags
+    if set -q _flag_t
+        for tag in "$_flag_t"
+            echo "$tag" >> "$entry_dir"/tags
+        end
+    else
+        touch "$entry_dir"/tags
+    end
+
+    if set -q _flag_T
+        echo "$_flag_T" > "$entry_dir"/title
+    else
+        touch "$entry_dir"/title
+    end
+
+    # Write template into new entry, which user will later on edit
+    set -l entry_text "$entry_dir"/body
+    set -l template __journal_entry_template 
+    "$template" >> "$entry_text"
+
+    # Store template in temporary file so we can
+    # easily compare the template with what the user
+    # edited. If the user simply closed the editor
+    # without making any changes, we can delete the
+    # entry again.
     set -l tmpfile (mktemp)
     "$template" > "$tmpfile"
 
-    if not test -n "$EDITOR" 
+    if not set -q EDITOR 
         echo '$EDITOR not set, exiting'
         exit 1
     end
-    "$EDITOR" "$fname"
 
-    if cmp "$fname" "$tmpfile"
+    "$EDITOR" "$entry_text"
+
+    # Check if files are different, meaning, check
+    # if user actually made any changes to the template
+    if cmp "$entry_text" "$tmpfile" > /dev/null
         echo "You didn't change the default template, so I'll delete the entry again"
-        rm "$fname"
+        rm "$entry_text"
     else
-        echo "Created new journal entry $fname"
+        echo "Created new journal entry $entry_text"
     end
 end
 
 function __journal_search
-    # Call with either -t or --tags
-    # Accepts multiple values separated with commas
-    set -l options (fish_opt -s t -l tags -r --multiple-vals)
+    set -l options                                 \
+        (fish_opt -s t -l tags -r --multiple-vals) \
+        (fish_opt -s T -l title -r)
     argparse $options -- $argv
 
-    if test -n "$_flag_t"
-        set -l tags (string split "," "$_flag_t")
-        set -l pattern (string join '|' $tags)
-        for match in (grep -R -Ewo -- $pattern $FISH_JOURNAL_DIR | awk -F ':' '{ print $1 }' | sort | uniq)
-            cat $match
-            echo ""
+    # For each category (tags, title), find all matches. Then return 
+    # the intersection of the matches. That's how this search works in a 
+    # nutshell and most of the code is plumbing and boilerplate since Fish 
+    # (obviously) doesn't have set functions (union, intersection, etc). If 
+    # a category is not defined, for example if the user did not supply 
+    # tags to search, consider all files to match. So if we have one match 
+    # for the given title, return the intersection of the title matches 
+    # (one file) and the tag matches (all files).
+    set -l tag_results $FISH_JOURNAL_DIR/*/tags
+
+    if set -q _flag_t
+        for tag in $_flag_t
+            set tag_results (grep -l "$tag" $tag_results)
         end
     end
 
-    # TODO: Add search by title
+    set -l title_results $FISH_JOURNAL_DIR/*/title
+
+    if set -q _flag_T
+        set title_results (grep -l "$_flag_T" $title_results)
+    end
+
+    # Remove the /title suffix so that we can compare matches from 
+    # different categories by dirname
+    set -l title_results_dirs
+    for path in $title_results
+        set -a title_results_dirs (dirname $path)
+    end
+
+    set -l results_dirs
+    for path in $tag_results
+        set -l dir (dirname $path)
+
+        if contains $dir $title_results_dirs
+            set -a results_dirs $dir
+        end
+    end
+
+    # Sort the results in descending order based on the date in each 
+    # entrys' "date" file. Use lexicographic sort thanks to the date format
+    set -l sorted (for v in $results_dirs; printf '%s "%s"\n' $v (cat $v/date); end | sort -k2 -r | awk '{ print $1 }')
+    
+    for path in $sorted
+        echo '--------------------'
+        echo $path
+        cat $path/date
+        cat $path/title
+        cat $path/body
+    end
 end
 
 function __journal_list_tags
@@ -90,12 +167,14 @@ function __journal_list_tags
     end
 end
 
-switch "$argv[1]"
-    case "tags"
-        __journal_list_tags
-    case "search"
-        set -e argv[1]
-        __journal_search $argv
-    case ""
-        __journal_new
+function journal -a cmd -d "Fish journal"
+    switch "$cmd"
+        case tags
+            __journal_list_tags
+        case search
+            __journal_search $argv
+        case \*
+            __journal_new $argv
+    end
 end
+
